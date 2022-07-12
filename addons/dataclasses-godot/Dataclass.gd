be8de3d95648c9a81eca451a6d5205cbdfe426ef
3ddelano <3ddelano@gmail.com>
2022-07-12 14:28:58 +0530

    feat: improve Dataclass printing (recursive)

--- addons/dataclasses-godot/Dataclass.gd
+++ addons/dataclasses-godot/Dataclass.gd
@@ -2,9 +2,9 @@
 # https://github.com/3ddelano/dataclasses-godot
 # MIT License
 # See License.md
-
 class_name Dataclass, "res://addons/dataclasses-godot/DataclassIcon.svg"
 extends Reference
+
 
 const __DEFAULT_OPTIONS__ = {
 	# Whether to sort properties when printing
@@ -20,23 +20,8 @@ const __DEFAULT_OPTIONS__ = {
 	"print_newline": false,
 
 	# Names of properties to exclude when printing
-	"print_exclude": [],
+	"print_exclude": []
 }
-
-
-var __name__: String
-var __options__: Dictionary
-
-
-func _init(p_name: String, p_options: Dictionary = {}):
-	__name__ = p_name
-	__options__ = __DEFAULT_OPTIONS__.duplicate()
-
-	# Override options
-	for key in p_options:
-		__options__[key] = p_options[key]
-
-	return self
 
 
 func from_dict(p_dict: Dictionary):
@@ -69,12 +54,30 @@ func to_dict() -> Dictionary:
 	return ret
 
 
+
+var __name__: String
+var __options__: Dictionary
+
+
+func _init(p_name: String, p_options: Dictionary = {}):
+	__name__ = p_name
+	__options__ = __DEFAULT_OPTIONS__.duplicate()
+	set_meta("is_dataclass", true)
+
+	# Override options
+	for key in p_options:
+		__options__[key] = p_options[key]
+
+	return self
+
+
 func get_class() -> String:
 	return __name__
 
 
 func __get_props() -> Array:
 	var all_props = get_property_list()
+	all_props.pop_front()
 	all_props.pop_front()
 	all_props.pop_front()
 	all_props.pop_front()
@@ -87,9 +90,59 @@ func __get_props() -> Array:
 	return all_props
 
 
-func __type_to_string(p_type: int) -> String:
-	var types = ["NIL", "BOOL", "INT", "REAL", "STRING", "VECTOR2", "RECT2", "VECTOR3", "TRANSFORM2D", "PLANE", "QUAT", "AABB", "BASIS", "TRANSFORM", "COLOR", "NODE_PATH", "RID", "OBJECT", "DICTIONARY", "ARRAY", "RAW_ARRAY", "INT_ARRAY", "REAL_ARRAY", "STRING_ARRAY", "VECTOR2_ARRAY", "VECTOR3_ARRAY", "COLOR_ARRAY", "MAX"]
-	return types[p_type]
+func __type_to_string(p_type: int, p_capital = true) -> String:
+	if p_capital:
+		return ["NIL", "BOOL", "INT", "REAL", "STRING", "VECTOR2", "RECT2", "VECTOR3", "TRANSFORM2D", "PLANE", "QUAT", "AABB", "BASIS", "TRANSFORM", "COLOR", "NODE_PATH", "RID", "OBJECT", "DICTIONARY", "ARRAY", "RAW_ARRAY", "INT_ARRAY", "REAL_ARRAY", "STRING_ARRAY", "VECTOR2_ARRAY", "VECTOR3_ARRAY", "COLOR_ARRAY", "MAX"][p_type]
+
+	return ["Nil", "Bool", "Int", "Real", "String", "Vector2", "Rect2", "Vector3", "Transform2D", "Plane", "Quat", "AABB", "Basis", "Transform", "Color", "NodePath", "RID", "Object", "Dictionary", "Array", "RawArray", "IntArray", "RealArray", "StringArray", "Vector2Array", "Vector3Array", "ColorArray", "Max"][p_type]
+
+
+func __variant_to_string(value, indent_level = 0) -> String:
+	var type = typeof(value)
+
+	var newline = ""
+	var tab = ""
+	var dict_join = ", "
+	if __options__.print_newline:
+		tab = "\t"
+		newline = "\n"
+		dict_join = ",\n"
+
+	match type:
+		TYPE_STRING:
+			return "\"" + value.c_escape() + "\""
+		TYPE_NIL, TYPE_INT, TYPE_REAL, TYPE_BOOL:
+			return value
+		TYPE_QUAT, TYPE_VECTOR2, TYPE_RECT2, TYPE_VECTOR3, TYPE_TRANSFORM2D, TYPE_BASIS:
+			return "%s%s"  % [__type_to_string(type, false), value]
+		TYPE_RID:
+			return "RID(%s)" % value.get_id()
+		TYPE_OBJECT:
+			if value == null:
+				return "Object(null)"
+			elif (not value is Script) and value.get_meta("is_dataclass"):
+				return str(value).indent(tab.repeat(indent_level)).trim_prefix(tab.repeat(indent_level))
+
+			return str(value)
+		TYPE_DICTIONARY:
+			var keys = value.keys()
+			var dict_string = PoolStringArray()
+			for key in keys:
+				dict_string.append("%s: %s" % [key, __variant_to_string(value[key], 0)])
+
+			dict_string = "{" + newline + dict_string.join(dict_join).indent(tab) + newline + "}"
+
+			return dict_string.indent(tab.repeat(indent_level)).trim_prefix(tab.repeat(indent_level))
+		TYPE_ARRAY, TYPE_RAW_ARRAY, TYPE_INT_ARRAY, TYPE_REAL_ARRAY, TYPE_STRING_ARRAY, TYPE_VECTOR2_ARRAY, TYPE_VECTOR3_ARRAY, TYPE_COLOR_ARRAY:
+			var ret = PoolStringArray()
+			for elm in value:
+				ret.append(__variant_to_string(elm, 1))
+			ret = "[" + ret.join(", ") + "]"
+			if type == TYPE_ARRAY: return ret
+
+			return __type_to_string(type, false) + "(" + ret + ")"
+		_:
+			return "%s(%s)"  % [__type_to_string(type, false), value]
 
 
 func _to_string() -> String:
@@ -99,12 +152,10 @@ func _to_string() -> String:
 	for prop in all_props:
 		if prop.name in __options__.print_exclude:
 			continue
-
 		var value = get(prop.name)
-		if typeof(value) == TYPE_STRING:
-			value = "\"" + value + "\""
+		var type = typeof(value)
 		if value != null or __options__.include_null_in_print:
-			props.append("%s=%s" % [prop.name, value])
+			props.append("%s = %s" % [prop.name, __variant_to_string(value, 1)])
 
 	var separator = ", "
 	var newline = ""
--- sample/Sample.gd
+++ sample/Sample.gd
@@ -1,9 +1,9 @@
 extends Control
 
-# A sample Dataclass
-class MyClass:
-	extends Dataclass
-	func _init().("MyClass"): pass
+
+# A basic Dataclass
+class MyClass extends Dataclass:
+	func _init().("MyClass", {print_newline = true}): return self
 
 	# Compulsory
 	var some_int: int
@@ -13,19 +13,117 @@ class MyClass:
 	var optional_dict = null
 
 
+# A Dataclass with nested objects
+class AnotherClass extends Dataclass:
+	func _init().("AnotherClass", {print_newline = true}): return self
+
+	var dict = {
+		dict2 = {
+			dict3 = {
+				int1 = 42,
+				dict4 = {
+					class_ = MyClass.new()
+				}
+			}
+		}
+	}
+
+
+# A sample class to showcase printing variables of all types
+class PrintingTest extends Dataclass:
+	func _init().("PrintingTest", {
+		sort_keys = false,
+		print_newline = true
+	}): return self
+
+	var var1 = null
+	var var2 = true
+	var var3 = 124
+	var var4 = 70.129
+	var var5 = "some string \n with newlines and \t tabs"
+	var var6 = Vector2(1, 2)
+	var var7 = Rect2(Vector2(1, 2), Vector2(10.2, 21.1))
+	var var8 = Vector3(1.9, 1.2, 3.1)
+	var var9 = Transform2D(12, Vector2(1, 0))
+	var var10 = Plane(0.1, 0.2, -1.2, -1.2)
+	var var11 = Quat(Vector3(1, 2, 3))
+	var var12 = AABB(Vector3(1,2,3), Vector3(9, -1, 1.0))
+	var var13 = Basis(var11)
+	var var14 = Transform(var11)
+	var var15 = Color(0.1, 0.2, 0.3)
+	var var16 = NodePath("Test")
+	var var17 = null # rid
+	var var18 = Object()
+	var var19 = { name = "123", num = 123, real = 1.23}
+	var var20 = [1, "asd", 12.2, {name = "key"}]
+	var var21 = PoolByteArray([1,2,1,2,2,2,1,2,3,32,3,12323,12,3,123,123,123,23,123,13,1231,23,12,132])
+	var var22 = PoolIntArray([1,2,3,23,1312,3,12312,312,321,321,312,312,312,312,321,321,312,321,312,32,41,42343,24])
+	var var23 = PoolRealArray([12.2, 31.2, 7.5])
+	var var24 = PoolStringArray(["a", "b", "d", "with \nnewline\tand tabs"])
+	var var25 = PoolVector2Array([Vector2(1,3), Vector2(9, 1.0)])
+	var var26 = PoolVector3Array([Vector3(1,2,3), Vector3(9, -1, 1.0)])
+	var var27 = PoolColorArray([Color("#222"), Color(123465)])
+	var var28 = MyClass.new()
+	var dict1 = {
+		nested_dataclass = MyClass.new(),
+		dict2 = {
+			int1 = 1,
+			dict3 = {
+				str1 = "hello",
+				int1 = 42,
+				nested_dataclass = AnotherClass.new()
+			}
+		},
+		nested_var_arr = [1.2, MyClass.new(), {str1 = "hello"}, [1, "string", 1.2]],
+		nested_var_arr2 = [1.2,  [1, "string", 1.2], 9.899999999]
+	}
+	var nodes = []
+
+
+# Customizing options for multiple classes
+# Extend the Dataclass
+class CustomDataclass extends Dataclass:
+
+	func _init(p_name = "AlwaysPrintNewlineDataclass").(p_name, {
+		sort_keys = false,
+		print_newline = true,
+		# ... etc any other options for
+	}): return self
+
+
+# Use the custom Dataclass
+class SomeClass extends CustomDataclass:
+	func _init().("SomeClass"): return self
+
+	var str1 = "hey"
+	var int1 = 12345
+	var arr1 = [1, "hello", 8.1]
+
+
 func _ready():
-	var obj = MyClass.new()
-	obj.optional_dict = {foo = "bar"}
 
 	# Better printing of objects
+	var obj = MyClass.new()
+	obj.optional_dict = {foo = "bar"}
 	print(obj)
+
 
 	# Create object from Dictionary
 	print(MyClass.new().from_dict({
 		some_int = 123456789
 	}))
 
+
 	# Serialize to Dictionary
 	var obj2 = MyClass.new().from_dict({some_array = [1,2,3,4, 5]})
 	print(obj2.to_dict())
 
+
+	# Printing test
+	var print_test = PrintingTest.new()
+	print_test.nodes.append_array([$Node, $Spatial, $Control, $Node2D])
+	print(print_test)
+
+
+	# Using a custom Dataclass
+	print(SomeClass.new())
